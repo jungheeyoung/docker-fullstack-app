3a9812a727ce821d2f7dfdb8dd5866dcba4364f9
heeyoung <heeyoung.jeong@naviworks.com>
2021-02-24 16:33:47 +0900

    travis ci deploy added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'navi0208!',
-    database: 'myapp'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 exports.pool = pool;
